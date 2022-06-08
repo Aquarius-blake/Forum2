e6087f321de2e75d5ac2bce50daedef48ee48c46
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-06-08 10:41:04 +0000

    INIT

---
+++ lib/Models/Users1.dart
@@ -0,0 +1,13 @@
+
+// Custom User Class
+class User1{
+
+  late final  String UID;
+  late final String Email;
+  late final String Password;
+  late final bool Guest;
+
+
+  User1({required this.UID});
+
+}
