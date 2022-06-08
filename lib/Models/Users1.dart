ff9d28b41e0594a7457869b62ed04d9cdcdf5e76
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-06-08 21:01:36 +0000

    INIT

--- lib/Models/Users1.dart
+++ lib/Models/Users1.dart
@@ -1,13 +1,16 @@
 
 // Custom User Class
+import 'package:flutter/material.dart';
+
 class User1{
 
   late final  String UID;
-  late final String Email;
-  late final String Password;
-  late final bool Guest;
+  late final String? Email;
+  late final String? Password;
+  late final bool? Guest;
+  late final Image? profilepic;
 
 
-  User1({required this.UID});
+  User1({required this.UID, this.profilepic,this.Guest,this.Email,this.Password});
 
 }
